3a06bb67abf11f09b30c27860ed94ee9c2538f9a
Ignashov Ivan <pohvalister@mail.ru>
2021-04-26 12:15:09 +0300

    initial commit

---
+++ CSVEvaluator.cpp
@@ -0,0 +1,7 @@
+﻿// CSVEvaluator.cpp: определяет точку входа для приложения.
+//
+
+#include "CSVEvaluator.h"
+
+using namespace std;
+
---
+++ CSVEvaluator.h
@@ -0,0 +1,69 @@
+﻿#ifndef CSV_EVALUATOR_H
+#define CSV_EVALUATOR_H
+
+#include <iostream>
+#include <variant>
+#include <unordered_map>
+#include <vector>
+
+//Table values operations enum
+enum OP{
+	ADD,
+	SUB,
+	MUL,
+	DIV
+};
+
+template<class TValue>
+class TableEvaluator;
+
+//Class to store table with formulas
+template<class TValue>
+class TableHolder {
+private:
+	//cells type definition
+	class Operation;
+	typedef std::variant<TValue, Operation> Cell;
+	
+	struct Operation {
+		Cell* fst;
+		Cell* snd;
+		OP op;
+	};
+
+	std::vector <std::vector<Cell>> data;
+
+	//rows and Columns
+	std::unordered_map<std::string, std::size_t> rows_names;
+	std::unordered_map<std::string, std::size_t> colomns_names;
+
+	friend class TableEvaluator;
+
+	//operations
+
+	Cell& get_cellstd::string, std::string);
+public:
+	//initialization
+	TableHolder();
+
+	void get_data_from_file(FILE* file); 
+
+	//
+	std::string get(std::string, std::string);
+};
+
+//Evaluator for given table
+template<class TValue>
+class TableEvaluator{
+
+	TableEvaluator(TableHolder<TValue>);
+
+	TableHolder<TValue>* evaluate();
+
+	std::vector<std::vector<TValue>>& evaluate();
+
+	bool is_solvable();
+};
+
+
+#endif //CSV_EVALUATOR_H
